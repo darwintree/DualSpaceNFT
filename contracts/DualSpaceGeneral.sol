2f411c9441f95d915fc7f843fdfd81ea646d7fa7
darwintree <daerwendeshu@outlook.com>
2023-07-24 18:23:35 +0800

    update tokenURI strategy(not deployed yet)

--- contracts/DualSpaceGeneral.sol
+++ contracts/DualSpaceGeneral.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 abstract contract DualSpaceGeneral is ERC721 {
 
@@ -16,7 +17,7 @@ abstract contract DualSpaceGeneral is ERC721 {
     }
 
     // token example 2023040101010001
-    // token batch * 10^8 + category * 10^6 + rarity * 10^4 + batch internal id
+    // token batch *  10^6 + rarity * 10^4 + batch internal id
     function _resolveTokenId(uint256 tokenId) internal pure returns (TokenMeta memory) {
         uint256 batchNbr = tokenId/(10**6);
         uint256 rarity = (tokenId - batchNbr * 10**6 )/10**4;
@@ -30,6 +31,18 @@ abstract contract DualSpaceGeneral is ERC721 {
 
     function isPrivilegeExpired(uint256 tokenId) public view returns (bool){
         return block.number > getPrivilegeExpiration(tokenId);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return "https://raw.githubusercontent.com/darwintree/dual-space-nft-token-uri-test/main/";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory baseURI = _baseURI();
+        // same batch nbr and same rarity has same metadata
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, Strings.toString(tokenId/10**4))) : "";
     }
 
     // should add?
